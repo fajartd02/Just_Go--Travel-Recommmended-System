1cbb90f4e95eea26c247fbc00e0575cab9098c64
unknown <fajarmh2@gmail.com>
2022-01-30 02:35:45 +0700

    Customize XML Sign In

--- Android App/app/src/main/java/com/fajar/mov/sign/SignInActivity.kt
+++ Android App/app/src/main/java/com/fajar/mov/sign/SignInActivity.kt
@@ -16,7 +16,7 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-        va
+
 
 //        val button_sign = findViewById<Button>(R.id.button_sign)
 //        button_sign.setOnClickListener {
